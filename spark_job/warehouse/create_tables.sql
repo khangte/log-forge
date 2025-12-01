acc84aabcdeb86b47c1d570168c0a485ce5700f1
khangte <minhyuk00321@gmail.com>
2025-12-01 15:36:04 +0900

    chore: dim 테이블 생성쿼리 주석처리 & 시간 관련 필드 utc 시간으로 변경

--- spark_job/warehouse/create_tables.sql
+++ spark_job/warehouse/create_tables.sql
@@ -3,8 +3,8 @@ CREATE DATABASE IF NOT EXISTS analytics;
 CREATE TABLE IF NOT EXISTS analytics.fact_log
 (
     -- 시간
-    event_ts   DateTime64(3)           ,  -- 실제 로그 시각 (애플리케이션에서 찍힌 timestamp)
-    ingest_ts  DateTime64(3)           ,  -- Spark → ClickHouse 적재 시각
+    event_ts   DateTime64(3, 'UTC')    ,  -- 애플리케이션 이벤트 UTC 시각
+    ingest_ts  DateTime64(3, 'UTC')    ,  -- Spark → ClickHouse 적재 시각(UTC)
 
     -- 공통 메타 정보
     service        LowCardinality(String),
@@ -17,10 +17,10 @@ CREATE TABLE IF NOT EXISTS analytics.fact_log
     event          String,
 
     -- 비즈니스 필드
-    user_id           String,
-    notification_type String,
-    product_id        Int32,
-    amount            Int32,
+    user_id           Nullable(String),
+    notification_type Nullable(String),
+    product_id        Nullable(Int32),
+    amount            Nullable(Int32),
 
     -- 인프라/추적용
     topic     String,       -- Kafka topic 이름
@@ -33,47 +33,47 @@ PARTITION BY toDate(event_ts)
 ORDER BY (service, event_ts, request_id);
 
 
-CREATE TABLE IF NOT EXISTS analytics.dim_service
-(
-    service       String,
-    service_group String,
-    is_active     UInt8
-)
-ENGINE = MergeTree
-ORDER BY service;
+-- CREATE TABLE IF NOT EXISTS analytics.dim_service
+-- (
+--     service       String,
+--     service_group String,
+--     is_active     UInt8
+-- )
+-- ENGINE = MergeTree
+-- ORDER BY service;
 
 
-CREATE TABLE IF NOT EXISTS analytics.dim_status_code
-(
-    status_code  Int32,
-    status_class String,
-    is_error     UInt8,
-    description  String
-)
-ENGINE = MergeTree
-ORDER BY status_code;
+-- CREATE TABLE IF NOT EXISTS analytics.dim_status_code
+-- (
+--     status_code  Int32,
+--     status_class String,
+--     is_error     UInt8,
+--     description  String
+-- )
+-- ENGINE = MergeTree
+-- ORDER BY status_code;
 
 
-CREATE TABLE IF NOT EXISTS analytics.dim_date
-(
-    date         Date,
-    year         UInt16,
-    month        UInt8,
-    day          UInt8,
-    week         UInt8,
-    day_of_week  UInt8,
-    is_weekend   UInt8
-)
-ENGINE = MergeTree
-ORDER BY date;
+-- CREATE TABLE IF NOT EXISTS analytics.dim_date
+-- (
+--     date         Date,
+--     year         UInt16,
+--     month        UInt8,
+--     day          UInt8,
+--     week         UInt8,
+--     day_of_week  UInt8,
+--     is_weekend   UInt8
+-- )
+-- ENGINE = MergeTree
+-- ORDER BY date;
 
 
-CREATE TABLE IF NOT EXISTS analytics.dim_time
-(
-    hour        UInt8,     -- 0~23
-    minute      UInt8,     -- 0~59
-    second      UInt8,     -- 0~59
-    time_of_day String     -- dawn/morning/afternoon/evening
-)
-ENGINE = MergeTree
-ORDER BY time_key;
+-- CREATE TABLE IF NOT EXISTS analytics.dim_time
+-- (
+--     hour        UInt8,     -- 0~23
+--     minute      UInt8,     -- 0~59
+--     second      UInt8,     -- 0~59
+--     time_of_day String     -- dawn/morning/afternoon/evening
+-- )
+-- ENGINE = MergeTree
+-- ORDER BY time_key;
