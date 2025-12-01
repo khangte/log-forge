5cc648ea305ee0aeea930cbedefd3dfd5a8b1641
khangte <minhyuk00321@gmail.com>
2025-12-01 15:56:15 +0900

    feat: isolationLevel 옵션 추가

--- spark_job/warehouse/sink.py
+++ spark_job/warehouse/sink.py
@@ -4,7 +4,7 @@ import traceback
 def write_to_clickhouse(df, table_name):
     try:
         print(f"[📥 ClickHouse] Writing to {table_name}")
-        df.show(5, truncate=False)
+        df.show(1, truncate=False)
 
         df.write \
             .format("jdbc") \
@@ -13,8 +13,16 @@ def write_to_clickhouse(df, table_name):
             .option("user", "log_user") \
             .option("password", "log_pwd") \
             .option("dbtable", table_name) \
+            .option("isolationLevel", "NONE") \
             .mode("append") \
             .save()
     except Exception as e:
         print(f"[❌ ERROR] Failed writing {table_name} to ClickHouse: {e}")
         traceback.print_exc()
+
+
+# --------------------------------------------------------------------------------
+# "WARN JdbcUtils: Requested isolation level 1, but transactions are unsupported" 로그 무시
+# 1. jdbcCompliant=false 옵션 추가
+# 2. isolationLevel=NONE 옵션 추가
+# --------------------------------------------------------------------------------
