6b00419092136fbeba481d13fa13864a0ac18bb7
khangte <minhyuk00321@gmail.com>
2025-12-18 09:55:33 +0900

    feat(clickhouse): grafana_user 권한 프로비저닝(system.query_log, agg 테이블 포함)

---
+++ spark_job/warehouse/clickhouse_grafana_grants.sql
@@ -0,0 +1,12 @@
+-- Grafana가 system.query_log 기반 대시보드를 조회할 수 있도록 권한 부여
+-- NOTE: ClickHouse 버전에 따라 system.* 접근이 기본적으로 제한될 수 있다.
+
+CREATE USER IF NOT EXISTS grafana_user IDENTIFIED BY 'grafana_pwd';
+
+-- 기존 로그 대시보드(analytics 스키마) 조회용
+GRANT SELECT ON analytics.fact_log_agg_1m TO grafana_user;
+GRANT SELECT ON analytics.fact_log_agg_event_1m TO grafana_user;
+GRANT SELECT ON analytics.fact_log TO grafana_user;
+
+-- 쿼리 모니터링 대시보드 조회용
+GRANT SELECT ON system.query_log TO grafana_user;
