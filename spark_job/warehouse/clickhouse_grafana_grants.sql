371d5a67dee760fa7d00e6050b04ac93b738287b
khangte <minhyuk00321@gmail.com>
2025-12-27 20:26:20 +0900

    chore(clickhouse): grafana_user 권한 테이블 추가

--- spark_job/warehouse/clickhouse_grafana_grants.sql
+++ spark_job/warehouse/clickhouse_grafana_grants.sql
@@ -6,9 +6,11 @@ CREATE USER IF NOT EXISTS grafana_user IDENTIFIED BY 'grafana_pwd';
 -- 기존 로그 대시보드(analytics 스키마) 조회용
 GRANT SELECT ON analytics.fact_log_agg_1m TO grafana_user;
 GRANT SELECT ON analytics.fact_log_agg_event_1m TO grafana_user;
+GRANT SELECT ON analytics.fact_log_latency_1m TO grafana_user;
+GRANT SELECT ON analytics.fact_log_lag_1m TO grafana_user;
+GRANT SELECT ON analytics.fact_log TO grafana_user;
 GRANT SELECT ON system.parts TO grafana_user;
 GRANT SELECT ON system.merges TO grafana_user;
-GRANT SELECT ON analytics.fact_log TO grafana_user;
 
 -- 쿼리 모니터링 대시보드 조회용
 GRANT SELECT ON system.query_log TO grafana_user;
